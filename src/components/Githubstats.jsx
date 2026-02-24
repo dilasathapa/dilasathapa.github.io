4063e1bf7e37ba733ee383fb0489bb4e4d3b7ef0
dilasathapa <thapadilasa71@gmail.com>
2026-02-24 18:21:36 +0530

    project section updates

--- src/components/Githubstats.jsx
+++ src/components/Githubstats.jsx
@@ -6,7 +6,7 @@ export const Githubstats = () => {
                     <h1>Github Statistics</h1>
                     <img id="first-git" src="https://github-profile-summary-cards.vercel.app/api/cards/profile-details?username=dilasathapa&amp;theme=dracula&amp;bg_color=0D1117" alt="githubstat" />
                     <div id="git-side">
-                        <img src="https://github-readme-stats.vercel.app/api?username=dilasathapa" alt="stats2" />
+                        {/* <img src="https://github-readme-stats.vercel.app/api?username=dilasathapa" alt="stats2" /> */}
                         <img src="https://github-readme-streak-stats.herokuapp.com/?user=dilasathapa" alt="stat3" />
                     </div>
                     {/* <img src="" alt="" /> */}
